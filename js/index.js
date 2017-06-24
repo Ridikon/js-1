2e0e4e29c7a72170fe9cbcd93a970a43cc4c9498
Vitalii <ridikon666@gmail.com>
2017-06-24 14:56:20 +0000

    Js

---
+++ js/index.js
@@ -0,0 +1,42 @@
+// alert('Hello World');
+// document.write('Hello World');
+// console.log('Hello World');
+
+// var userName = prompt('Enter your name');
+// document.write('Hello ' + userName);
+
+// var userAge = Number(prompt('Enter your age'));
+
+// document.write('<br>');
+
+// document.write('your age ' + userAge);
+
+// document.write('<br>');
+
+// var newAge = sumTree(userAge, 1, 1);//userAge + 1;
+
+// document.write('<br>');
+// document.write('next year you will be ' + newAge);
+
+// document.write('<br>');
+// document.write('You are ' + checkAge(userAge));
+
+// function sum(a, b) {
+//     var result = a + b;
+//     return result;
+// }
+
+// function sumTree(a, b, c) {
+//     var result = a+ b + c;
+//     return result;
+// }
+
+// function checkAge(age) {
+//     if (age < 35) {
+//         return 'young';
+//     } else {
+//         return 'not young';
+//     }
+// }
+
+document.write(Math.sqrt(4));
